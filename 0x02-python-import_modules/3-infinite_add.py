923e59734487b9aef0c4dd4518b3c57eb3671942
Hamza Yassine <ihamzayassine@gmail.com>
2024-01-04 21:05:34 +0100

    Task 3 done

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+
+summ = 0
+if len(argv) == 1:
+    print("0")
+elif len(argv) > 1:
+    for i in range(1, len(argv)):
+        summ += int(argv[i])
+print("{}".format(summ))
