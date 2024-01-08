d9c0039cbc80ad0653edd5343b5b2e4fb154821d
Hamza Yassine <ihamzayassine@gmail.com>
2024-01-08 19:28:24 +0100

    Task 9 done

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return 'None'
+    else:
+        maxvalue = my_list[0]
+        for i in range(len(my_list)):
+            if my_list[i] >= maxvalue:
+                maxvalue = my_list[i]
+        return maxvalue
