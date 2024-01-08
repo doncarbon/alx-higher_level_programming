da056eb6f3b554a259162807bc03f3ff437cb723
Hamza Yassine <ihamzayassine@gmail.com>
2024-01-08 19:08:18 +0100

    Task 8 done

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,2 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
+    if len(sentence) == 0:
+        rvalues = 0, "None"
+    else:
+        rvalues = len(sentence), sentence[0]
+    return rvalues
