df46e800294e1e2fdeacbfc345b4002d4c14ac52
Hamza Yassine <ihamzayassine@gmail.com>
2024-01-23 23:40:07 +0100

    Task 1 done

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""Define a Square Class."""
+
+
+class Square:
+    """Square Class"""
+
+    def __init__(self, size):
+        """Initialization of a new Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
