63d9e9a150dacdc2cb4e62efb8526ae62b8d612e
Hamza Yassine <ihamzayassine@gmail.com>
2024-01-27 02:52:32 +0100

    Task 5 done, testing file is still in progress.

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Defines that prints a text with 2 new lines after each of these characters: ., ? and :"""
+
+
+def text_indentation(text):
+    """Prints a text with 2 new lines after each of these characters: ., ? and :
+
+    Args:
+        text (string): the text to indent.
+
+    Raises:
+        TypeError: If the text isn't a string.
+
+    Return:
+        Nothing.
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    delim = ['.', '?', ':']
+
+    text_list = list(text)
+
+    for char in range(0, len(text_list)):
+        for i in range(0, len(delim)):
+            if text_list[char] == delim[i]:
+                if text_list[char + 1] == " ":
+                    text_list[char + 1] = "\n\n"
+                elif text_list[char + 1] == "":
+                    text_list[char + 1] = ""
+                else:
+                    text_list[char] += "\n\n"
+
+    new_text = ''.join(text_list)
+
+    print(new_text)
