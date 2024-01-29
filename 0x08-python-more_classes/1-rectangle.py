2d3fa7ecadb56d58b704c3b8c25ac1d4f018390b
Hamza Yassine <ihamzayassine@gmail.com>
2024-01-29 22:12:40 +0100

    Task 1 fix

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -4,6 +4,7 @@
 
 class Rectangle:
     """Rectangle Class"""
+
     def __init__(self, width=0, height=0):
         """Initialization of a new Square.
 
@@ -28,7 +29,7 @@ class Rectangle:
         """
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
@@ -46,6 +47,6 @@ class Rectangle:
         """
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
