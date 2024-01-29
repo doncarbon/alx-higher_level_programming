fc2e23492aec463932c5fd259e56083e00ba8f06
Hamza Yassine <ihamzayassine@gmail.com>
2024-01-29 20:31:16 +0100

    Attribute documentation updated

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -7,7 +7,7 @@ class Rectangle:
 
     Attributes:
         number_of_instances (int): The number of Rectangle instances.
-        print_symbol (str): The symbol for string representation.
+        print_symbol (any): The symbol for string representation.
         """
 
     number_of_instances = 0
