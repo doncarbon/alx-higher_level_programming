a829e3a4fba523b0c5bc70024a1e289642e0e28b
Hamza Yassine <ihamzayassine@gmail.com>
2024-02-05 23:19:58 +0100

    Task 6 done

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Defines an Empty BaseGeometry Class."""
+
+
+class BaseGeometry():
+    """BaseGeometry class."""
+    def __init__(self):
+        """Empty initialization"""
+        pass
+
+    def area(self):
+        """Area method (still not implemented)"""
+        raise Exception("area() is not implemented")
