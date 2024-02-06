954f3feeaf4ba10af120e1cb5649695e181eadd2
Hamza Yassine <ihamzayassine@gmail.com>
2024-02-06 20:35:53 +0100

    Task 0 done

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Defines a read file function."""
+
+
+def read_file(filename=""):
+    """
+    Reads a text file (UTF8) and prints it to stdout.
+
+    Args:
+        filename (str): the path to the file.
+    """
+    with open(filename, encoding="UTF-8") as file:
+        print(file.read())
---
+++ 0x0B-python-input_output/README.md
@@ -0,0 +1,7 @@
+# 0x0B. Python - Input/Output
+
+Welcome to the 0x0B. Python - Input/Output project! This project is part of the ALX Software Engineering curriculum.
+
+## About the Author
+- **Name:** Hamza YASSINE
+- **ALX Software Engineering Student** 
