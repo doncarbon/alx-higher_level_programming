913580756c450d3fc36cfdb689a6da025e2f0f26
Hamza Yassine <ihamzayassine@gmail.com>
2024-02-06 22:08:54 +0100

    Task 7 done

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Defines a script that add all arguments to a Python list and save them to a file."""
+import sys
+save_to_json_file = __import__("5-save_to_json_file.py").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file.py").load_from_json_file
+
+
+def main():
+    try:
+        jsonlist = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        jsonlist = []
+
+    for arg in range(len(sys.argv)):
+        jsonlist.append(arg)
+
+    save_to_json_file(jsonlist, "add_item.json")
+
+
+if __name__ == "__main__":
+    main()
