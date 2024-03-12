0355f60b9563a76bc182d8fd20898f3ebf328e55
Hamza Yassine <ihamzayassine@gmail.com>
2024-03-12 23:29:45 +0000

    Task 1 fix

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,3 +1,3 @@
 -- SQL script that creates the database hbtn_0c_0 in your MySQL server.
 --   If the database hbtn_0c_0 already exists, script should not fail.
-CREATE TABLE hbtn_0c_0;
+CREATE DATABASE IF NOT EXISTS `hbtn_0c_0`;
