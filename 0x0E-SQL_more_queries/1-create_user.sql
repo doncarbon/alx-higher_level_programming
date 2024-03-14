34f1998c2a8bdf1d2c7828949ba46dd9552ac304
Hamza Yassine <ihamzayassine@gmail.com>
2024-03-14 03:10:49 +0000

    Task 1 update, Task 2 done

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- SQL script that creates the MySQL server user user_0d_1.
-CREATE USER 'user_0d_1'@'hostname' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL ON *.* TO 'user_0d_1'@'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO user_0d_1@localhost;
---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,5 @@
+-- SQL script that creates the database hbtn_0d_2 and the user user_0d_2.
+CREATE DATABASE IF NOT EXISTS `hbtn_0c_2`;
+
+CREATE USER IF NOT EXISTS user_0d_2@hostname IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0c_2.* TO user_0d_2@localhost;
